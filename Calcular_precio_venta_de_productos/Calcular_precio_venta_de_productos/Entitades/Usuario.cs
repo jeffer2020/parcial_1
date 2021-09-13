1849122949d1ce4789e35ef4748726cc55af005c
jeffer2020 <rc0113032021@unab.edu.sv>
2021-09-13 15:09:53 -0600

    parcial_2

--- Calcular_precio_venta_de_productos/Calcular_precio_venta_de_productos/Entitades/Usuario.cs
+++ Calcular_precio_venta_de_productos/Calcular_precio_venta_de_productos/Entitades/Usuario.cs
@@ -8,5 +8,19 @@ namespace Calcular_precio_venta_de_productos.Entitades
 {
     class Usuario
     {
+        private string Nombre;
+        private int Contraseña;
+
+        public string Nombre1 { get => Nombre; set => Nombre = value; }
+        public int Contraseña1 { get => Contraseña; set => Contraseña = value; }
+
+
+        public Usuario(string nombre, int contraseña)
+        {
+            Nombre1 = nombre;
+            Contraseña1 = contraseña;
+        }
+
+       public Usuario() { }
     }
 }
--- Calcular_precio_venta_de_productos/Calcular_precio_venta_de_productos/Entitades/Venta.cs
+++ Calcular_precio_venta_de_productos/Calcular_precio_venta_de_productos/Entitades/Venta.cs
@@ -8,5 +8,28 @@ namespace Calcular_precio_venta_de_productos.Entitades
 {
     class Venta
     {
+        private string identificador_deproducto;
+        private string nombre_del_producto;
+        private string descripcion_del_producto;
+        private double precio_de_lproducto;
+        private int cantidad_del_producto;
+
+        public string Identificador_deproducto { get => identificador_deproducto; set => identificador_deproducto = value; }
+        public string Nombre_del_producto { get => nombre_del_producto; set => nombre_del_producto = value; }
+        public string Descripcion_del_producto { get => descripcion_del_producto; set => descripcion_del_producto = value; }
+        public double Precio_de_lproducto { get => precio_de_lproducto; set => precio_de_lproducto = value; }
+        public int Cantidad_del_producto { get => cantidad_del_producto; set => cantidad_del_producto = value; }
+
+
+        public Venta(string identificador_deproducto, string nombre_del_producto, string descripcion_del_producto, double precio_de_lproducto, int cantidad_del_producto)
+        {
+            this.Identificador_deproducto = identificador_deproducto;
+            this.Nombre_del_producto = nombre_del_producto;
+            this.Descripcion_del_producto = descripcion_del_producto;
+            this.Precio_de_lproducto = precio_de_lproducto;
+            this.Cantidad_del_producto = cantidad_del_producto;
+        }
+
+        
     }
 }
