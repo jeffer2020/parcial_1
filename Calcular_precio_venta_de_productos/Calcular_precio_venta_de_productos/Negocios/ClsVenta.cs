99a3b97bf0374115a947f0ff6c2e2b8729fe12ff
jeffer2020 <rc0113032021@unab.edu.sv>
2021-09-13 17:49:48 -0600

    123

--- Calcular_precio_venta_de_productos/Calcular_precio_venta_de_productos/Negocios/ClsVenta.cs
+++ Calcular_precio_venta_de_productos/Calcular_precio_venta_de_productos/Negocios/ClsVenta.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calcular_precio_venta_de_productos.Entitades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,23 @@ namespace Calcular_precio_venta_de_productos.Negocios
 {
     class ClsVenta
     {
+        Venta venta = new Venta();
+
+        public string funcionCobro(double precio, int cantidad)
+        {
+
+            double total = precio * cantidad;
+            if (total > 50)
+            {
+                double y = total * 10 / 100;
+                double K = total - y;
+                return "su compra tiene descueto del 10% por compras de mas de 50 dolares" +
+                    "su total a canselar sin descuento es de" + total + "su producto con el descuesto es de" + K;
+            }
+            else
+                return "su total a pagar sin descuento es de" + total; 
+            
+
+        }
     }
 }
