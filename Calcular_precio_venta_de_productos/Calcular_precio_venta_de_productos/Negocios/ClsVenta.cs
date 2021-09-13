9dc480d935f39722fac40735fbf3ce56a553836c
jeffer2020 <rc0113032021@unab.edu.sv>
2021-09-13 14:50:20 -0600

    parcial

---
+++ Calcular_precio_venta_de_productos/Calcular_precio_venta_de_productos/Negocios/ClsVenta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular_precio_venta_de_productos.Negocios
+{
+    class ClsVenta
+    {
+    }
+}
