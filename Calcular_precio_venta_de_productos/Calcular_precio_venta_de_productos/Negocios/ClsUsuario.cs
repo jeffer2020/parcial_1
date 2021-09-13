d32bce8d0371572ef6be8915424a77f6fc2646be
jeffer2020 <rc0113032021@unab.edu.sv>
2021-09-13 16:51:34 -0600

    usuario

--- Calcular_precio_venta_de_productos/Calcular_precio_venta_de_productos/Negocios/ClsUsuario.cs
+++ Calcular_precio_venta_de_productos/Calcular_precio_venta_de_productos/Negocios/ClsUsuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calcular_precio_venta_de_productos.Entitades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace Calcular_precio_venta_de_productos.Negocios
 {
     class ClsUsuario
     {
+        Usuario usuario = new Usuario();
+        public string 
+
     }
 }
